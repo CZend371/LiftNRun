19535628efe706af358740f2749b52cc63875efb
Cristian371 <czend371@gmail.com>
2019-09-17 21:52:04 -0500

    switched to new stopwatch package

--- client/src/components/timer/Timer.js
+++ client/src/components/timer/Timer.js
@@ -1,38 +1,51 @@
-import React from "react";
-import ReactStopwatch from "react-stopwatch";
+import React, { Component } from "react";
+import TimerMachine from "react-timer-machine"
+import moment from "moment";
+import momentDurationFormatSetup from "moment-duration-format";
 import "../timer/style.scss"
 
-// handleStart(){
 
-// }
+{/* <button type="button" className="btn btn-success" id="start">Start</button>
+    <button type="button" className="btn btn-secondary" id="pause">Pause</button>
+    <button type="button" className="btn btn-danger" id="stop">Stop</button> */}
 
-// handleStop() {
+momentDurationFormatSetup(moment);
 
-// }
-// handlePause() {
-
-// }
-const Timer = () => (
-    <ReactStopwatch
-        seconds={0}
-        minutes={0}
-        hours={0}
-        limit="60:60:60"
-        onChange={({ hours, minutes, seconds }) => {
-            // do something
-        }}
-        onCallback={() => console.log('Finish')}
-        render={({ formatted, hours, minutes, seconds }) => {
-            return (
-                <div>
-                    <p>
-                        Time: {formatted}
-                    </p>
-                </div>
-            );
-        }}
-    />
-);
+class Timer extends Component {
+    render() {
+        return (
+            <TimerMachine
+                timeStart={10 * 1000} // start at 10 seconds
+                timeEnd={20 * 1000} // end at 20 seconds
+                started={true}
+                paused={false}
+                countdown={false} // use as stopwatch
+                interval={1000} // tick every 1 second
+                formatTimer={(time, ms) =>
+                    moment.duration(ms, "milliseconds").format("h:mm:ss.SSS")
+                }
+                onStart={time =>
+                    console.info(`Timer started: ${JSON.stringify(time)}`)
+                }
+                onStop={time =>
+                    console.info(`Timer stopped: ${JSON.stringify(time)}`)
+                }
+                onTick={time =>
+                    console.info(`Timer ticked: ${JSON.stringify(time)}`)
+                }
+                onPause={time =>
+                    console.info(`Timer paused: ${JSON.stringify(time)}`)
+                }
+                onResume={time =>
+                    console.info(`Timer resumed: ${JSON.stringify(time)}`)
+                }
+                onComplete={time =>
+                    console.info(`Timer completed: ${JSON.stringify(time)}`)
+                }
+            />
+        )
+    }
+}
 
 export default Timer;
 
--- client/src/pages/Run/Run.js
+++ client/src/pages/Run/Run.js
@@ -7,7 +7,6 @@ import Timer from "../../components/timer/Timer";
 // Need to add a timer and buttons for start,pause,stop
 // Possible ideas: user should be able to switch between long distance runnig and sprints/intervals
 
-
 function Run() {
     // console.log(process.env.REACT_APP_GOOGLE_KEY);
     // console.log(`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_KEY}&callback=initMap`)
@@ -15,13 +14,10 @@ function Run() {
         <div className="container">
             <div className="timer">
                 <Timer />
-                <button type="button" className="btn btn-success" id="start">Start</button>
-                <button type="button" className="btn btn-secondary" id="pause">Pause</button>
-                <button type="button" className="btn btn-danger" id="stop">Stop</button>
             </div>
             <div className="map-container">
                 <div style={{ width: "985px", height: "500px" }}>
-                    <WrappedMap googleMapURL={`https://maps.googleapis.com/maps/api/js?&key=${process.env.REACT_APP_GOOGLE_KEY}&callback=initMap`}
+                    <WrappedMap googleMapURL={"https://maps.googleapis.com/maps/api/js?&key=AIzaSyAsh_FqlodCd1Kid-SEkq80YG-CAob0CTc&callback=initMap"}
                         loadingElement={<div style={{ height: "100%" }} />}
                         containerElement={<div style={{ height: "100%" }} />}
                         mapElement={<div style={{ height: "100%" }} />}
@@ -31,6 +27,5 @@ function Run() {
         </div>
     )
 }
-
 
 export default Run;
