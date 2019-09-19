66e1ad000c05a74c38bdc30489cb290f20eb49b6
Cristian371 <czend371@gmail.com>
2019-09-19 10:44:53 -0500

    Timer component is working now

--- client/src/components/timer/Timer.js
+++ client/src/components/timer/Timer.js
@@ -1,48 +1,68 @@
 import React, { Component } from "react";
-import TimerMachine from "react-timer-machine"
 import moment from "moment";
 import momentDurationFormatSetup from "moment-duration-format";
 import "../timer/style.scss"
 
 
-{/* <button type="button" className="btn btn-success" id="start">Start</button>
-    <button type="button" className="btn btn-secondary" id="pause">Pause</button>
-    <button type="button" className="btn btn-danger" id="stop">Stop</button> */}
+
 
 momentDurationFormatSetup(moment);
 
 class Timer extends Component {
+    state = {
+        timerStart: 0,
+        timerTime: 0,
+        isRunning: false
+    };
+
+    handleStart = () => {
+        this.setState({
+            isRunning: true,
+            timerTime: this.state.timerTime,
+            timerStart: Date.now() - this.state.timerTime
+        });
+        this.timer = setInterval(() => {
+            this.setState({
+                timerTime: Date.now() - this.state.timerStart
+            });
+        }, 10);
+    };
+
+    handleStop = () => {
+        this.setState({ isRunning: false });
+        clearInterval(this.timer);
+    };
+
+    handleReset = () => {
+        this.setState({
+            timerStart: 0,
+            timerTime: 0
+        })
+    }
+
+
     render() {
+        const { timerTime } = this.state;
+        let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
+        let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
+        let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
+        let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
         return (
-            <TimerMachine
-                timeStart={10 * 1000} // start at 10 seconds
-                timeEnd={20 * 1000} // end at 20 seconds
-                started={true}
-                paused={false}
-                countdown={false} // use as stopwatch
-                interval={1000} // tick every 1 second
-                formatTimer={(time, ms) =>
-                    moment.duration(ms, "milliseconds").format("h:mm:ss.SSS")
-                }
-                onStart={time =>
-                    console.info(`Timer started: ${JSON.stringify(time)}`)
-                }
-                onStop={time =>
-                    console.info(`Timer stopped: ${JSON.stringify(time)}`)
-                }
-                onTick={time =>
-                    console.info(`Timer ticked: ${JSON.stringify(time)}`)
-                }
-                onPause={time =>
-                    console.info(`Timer paused: ${JSON.stringify(time)}`)
-                }
-                onResume={time =>
-                    console.info(`Timer resumed: ${JSON.stringify(time)}`)
-                }
-                onComplete={time =>
-                    console.info(`Timer completed: ${JSON.stringify(time)}`)
-                }
-            />
+            <div className="timer-container">
+                <div className="display">{hours}:{minutes}:{seconds}:{centiseconds}</div>
+                {this.state.isRunning === false && this.state.timerTime === 0 && (
+                    <button type="button" className="btn btn-success" id="start" onClick={this.handleStart}>Start</button>
+                )}
+                {this.state.isRunning === true && (
+                    <button type="button" className="btn btn-secondary" id="stop" onClick={this.handleStop}>Stop</button>
+                )}
+                {this.state.isRunning === false && this.state.timerTime > 0 && (
+                    <button type="button" className="btn btn-success" id="resue" onClick={this.handleStart}>Resume</button>
+                )}
+                {this.state.isRunning === false && this.state.timerTime > 0 && (
+                    <button type="button" className="btn btn-danger" id="reset" onClick={this.handleReset}>Reset</button>
+                )}
+            </div>
         )
     }
 }
