4700269ec07544055fa8c7fbc37281dac8d17ed9
Htran_14 <hao.allen.tran@gmail.com>
2019-09-24 20:37:15 -0500

    Update Profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -5,9 +5,9 @@ import "./style.css";
 
 class Profile extends Component {
     state = {
-        Name: "",
-        Intro: "",
-        Location: "",
+        name: "",
+        intro: "",
+        location: "",
         savedWorkout: []
     };
     componentDidMount() {
@@ -19,22 +19,22 @@ class Profile extends Component {
         var check = document.getElementById("expandHiistory").getAttribute('isExpanded');
         if (check === true) {
             document.getElementById("expandHiistory").setAttribute('isFront', false);
-            document.getElementById("HistoryGroup").classList.add('historyGroupHide ');
-            document.getElementById("HistoryGroup ").classList.remove('historyGroupShow');
+            document.getElementById("HistoryGroup").classList.add('historyGroupShow');
+            document.getElementById("HistoryGroup ").classList.remove('historyGroupHide');
 
         } else {
             document.getElementById("expandHiistory").setAttribute('isFront', true);
-            document.getElementById("HistoryGroup").classList.add('historyGroupShow ');
-            document.getElementById("HistoryGroup ").classList.remove('historyGroupHide');
+            document.getElementById("HistoryGroup").classList.add('historyGroupHide');
+            document.getElementById("HistoryGroup ").classList.remove('historyGroupShow');
         }
     }
     render() {
         return (
             <div>
                 <div id="profileGroup">
-                    <div id="profileName">{this.state.Name}</div>
-                    <div id="profileIntro">{this.state.Name}</div>
-                    <div id="profileLocation">{this.state.Name}</div>
+                    <div id="profileName">{this.state.name}</div>
+                    <div id="profileIntro">{this.state.intro}</div>
+                    <div id="profileLocation">{this.state.location}</div>
                 </div>
                 <div id="expandHiistory" class="expandBtn" onClick={this.ExpandHistory} isExpanded="true"></div>
                 <div id="HistoryGroup" class="historyGroupHide">
