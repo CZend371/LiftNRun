a6ed56ccc73521998fccfdbca40b08315492fe80
donxiya <31514669+donxiya@users.noreply.github.com>
2019-09-29 19:13:54 -0500

    login style

--- client/src/pages/Login/LoginPage/SignUp.js
+++ client/src/pages/Login/LoginPage/SignUp.js
@@ -58,14 +58,14 @@ class Signup extends Component {
                 <form name="form" onSubmit={this.handleSubmit}>
                     <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
                         <label className="label" htmlFor="username">Username</label>
-                        <input type="text" className="form-control" name="username" value={username} onChange={this.handleChange} />
+                        <input type="text" className="input form-control" name="username" value={username} onChange={this.handleChange} />
                         {submitted && !username &&
                             <div className="help-block">Username is required</div>
                         }
                     </div>
                     <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
                         <label className="label" htmlFor="password">Password</label>
-                        <input type="password" className="form-control" name="password" value={password} onChange={this.handleChange} />
+                        <input type="password" className="input form-control" name="password" value={password} onChange={this.handleChange} />
                         {submitted && !password &&
                             <div className="help-block">Password is required</div>
                         }
