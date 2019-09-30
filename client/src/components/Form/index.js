c6f94787e030d72ad8fe3f0bc9d1cd81159d98ef
donxiya <31514669+donxiya@users.noreply.github.com>
2019-09-29 21:55:47 -0500

    form fix

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -8,7 +8,7 @@ import "../../style/main.scss";
 export class NumbericInput extends Component {
     render() {
         return (
-            <input className="Input" {...props} />
+            <input className="Input" {...this.props} />
         )
     }
 }
@@ -17,7 +17,7 @@ export class NameInput extends Component {
     render() {
         return (
 
-            <input className="Input" {...props} />
+            <input className="Input" {...this.props} />
 
         )
     }
@@ -25,7 +25,7 @@ export class NameInput extends Component {
 
 export function FormBtn() {
     return (
-        <button {...props} className="FormBtn">
+        <button className="FormBtn" >
             Submit
         </button>
     );
