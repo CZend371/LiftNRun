c5606f709c8163f55902705a234f2192649a2be4
Cristian371 <czend371@gmail.com>
2019-09-30 09:59:44 -0500

    centered buttons on the menu page, and centered text on home page.

--- client/src/components/BinarySelection/index.js
+++ client/src/components/BinarySelection/index.js
@@ -31,8 +31,9 @@ export default class BinarySelection extends Component {
     render() {
         return (
             <div>
-                <a id="topLink" href={this.props.topLink}>
-                    <div id="topBtn" className="topBtn binaryBtn" onClick={this.clickTop}>{this.props.topTitle}</div></a>
+                <a id="topLink" href={this.props.topLink} >
+                    <div id="topBtn" className="topBtn binaryBtn" onClick={this.clickTop}>{this.props.topTitle}
+                    </div></a>
                 <a id="bopLink" href={this.props.botLink}>
                     <div id="botBtn" className="botBtn binaryBtn" onClick={this.clickBot}>{this.props.botTitle}</div></a>
             </div>
