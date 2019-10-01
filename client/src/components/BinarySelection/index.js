a277c5f0c611c5f9a642a132fc924231154c459f
donxiya <31514669+donxiya@users.noreply.github.com>
2019-10-01 01:54:55 -0500

    link

--- client/src/components/BinarySelection/index.js
+++ client/src/components/BinarySelection/index.js
@@ -21,10 +21,10 @@ export default class BinarySelection extends Component {
     }
     clickBot = event => {
         if (this.state.bot === false){
-            var b = document.getElementById('botLink');
-            b.href = this.props.botLink;
+
             var a = document.getElementById('topLink');
             a.href = "#";
+            document.getElementById("botBtn").href = this.props.botLink;
             this.setState({ top: false, bot: true });
             document.getElementById("topBtn").style.height = "262px";
             document.getElementById("botBtn").style.height = "530px";
