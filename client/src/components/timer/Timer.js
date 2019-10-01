5939a4b862e7b76c7b8d81dfce8a4cd2eed9a46a
donxiya <31514669+donxiya@users.noreply.github.com>
2019-09-30 23:47:00 -0500

    Update Timer.js

--- client/src/components/timer/Timer.js
+++ client/src/components/timer/Timer.js
@@ -59,7 +59,7 @@ class Timer extends Component {
         let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
         let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
         return (
-            <div className="timer-container">
+            <div className="timerLoc timer-container">
                 <div className="display">{hours}:{minutes}:{seconds}:{centiseconds}</div>
                 {this.state.isRunning === false && this.state.timerTime === 0 && (
                     <div type="button" className="bigBtn btn btn-success" id="start" onClick={this.handleStart}>Start</div>
